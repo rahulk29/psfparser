07854da8f9d3041b7af412243a20b9ca17881fdd
Rahul Kumar <rahulkumar@berkeley.edu>
2023-05-14 15:36:08 -0700

    Add checks to test

--- src/tests.rs
+++ src/tests.rs
@@ -11,6 +11,14 @@ fn parses_vdiv_sin() {
     let ascii_data = TransientData::from_ascii(&ast);
     let ast = bin_parse(VDIV_SIN_PSFBIN).expect("Failed to parse transient PSF file");
     let bin_data = TransientData::from_binary(ast);
+    assert_eq!(bin_data.signals.len(), 4);
+    assert_eq!(
+        bin_data
+            .signal("time")
+            .expect("should contain a time signal")
+            .len(),
+        16001
+    );
 
     assert!(ascii_data.approx_eq(&bin_data, 1e-12));
 }
